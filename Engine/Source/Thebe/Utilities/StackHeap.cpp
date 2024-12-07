193c4e73beb0bd275f90475e1c19271e656f61dd
Spencer T. Parkin <spencer.parkin@proton.me>
2024-12-07 12:40:23 -0700

    This might come in handy for something like particles, maybe.

--- Engine/Source/Thebe/Utilities/StackHeap.cpp
+++ Engine/Source/Thebe/Utilities/StackHeap.cpp
@@ -1,1 +1,76 @@
 #include "Thebe/Utilities/StackHeap.h"
+#include "Thebe/Log.h"
+
+using namespace Thebe;
+
+StackHeap::StackHeap()
+{
+	this->memoryBuffer = nullptr;
+	this->memoryBufferSize = 0;
+	this->blockSize = 0;
+	this->blockStackTop = 0;
+}
+
+/*virtual*/ StackHeap::~StackHeap()
+{
+}
+
+bool StackHeap::SetManagedMemory(uint8_t* memoryBuffer, uint64_t memoryBuffersize, uint64_t blockSize)
+{
+	this->memoryBuffer = memoryBuffer;
+	this->memoryBufferSize = memoryBufferSize;
+	this->blockSize = blockSize;
+
+	if (this->blockSize > this->memoryBufferSize)
+	{
+		THEBE_LOG("The block size must not exceed that of the buffer.");
+		return false;
+	}
+
+	if (this->memoryBufferSize % this->blockSize != 0)
+	{
+		THEBE_LOG("The memory buffer size must be a multiple of the block size.");
+		return false;
+	}
+
+	this->blockStack.clear();
+	uint64_t numBlocks = this->memoryBufferSize / this->blockSize;
+	this->blockStack.resize(numBlocks);
+	for (uint64_t i = 0; i < numBlocks; i++)
+		this->blockStack[i] = i * blockSize;
+
+	this->blockStackTop = 0;
+	return true;
+}
+
+uint8_t* StackHeap::AllocateBlock()
+{
+	if (this->blockStackTop == (uint64_t)this->blockStack.size())
+	{
+		THEBE_LOG("Stack overflow!  No more memory can be allocated from the stack heap.");
+		return nullptr;
+	}
+
+	uint64_t blockOffset = this->blockStack[this->blockStackTop++];
+	uint8_t* block = &this->memoryBuffer[blockOffset];
+	return block;
+}
+
+bool StackHeap::DeallocateBlock(uint8_t* block)
+{
+	uint64_t blockOffset = this->memoryBuffer - block;
+	if (blockOffset + this->blockSize > this->memoryBufferSize || blockOffset % this->blockSize != 0)
+	{
+		THEBE_LOG("Invalid pointer (0x%016x) given to stack heap.", uintptr_t(block));
+		return false;
+	}
+
+	if (this->blockStackTop == 0)
+	{
+		THEBE_LOG("Stack underflow!  All memory is already free in the stack heap.");
+		return false;
+	}
+
+	this->blockStack[--this->blockStackTop] = blockOffset;
+	return true;
+}
--- Engine/Source/Thebe/Utilities/StackHeap.h
+++ Engine/Source/Thebe/Utilities/StackHeap.h
@@ -1,1 +1,70 @@
 #pragma once
+
+#include "Thebe/Common.h"
+#include <vector>
+
+namespace Thebe
+{
+	/**
+	 * This is a heap that provides O(1) allocate and deallocation by
+	 * virtue of the fact that every allocation is the same size.
+	 */
+	class THEBE_API StackHeap
+	{
+	public:
+		StackHeap();
+		virtual ~StackHeap();
+
+		bool SetManagedMemory(uint8_t* memoryBuffer, uint64_t memoryBuffersize, uint64_t blockSize);
+
+		uint8_t* AllocateBlock();
+		bool DeallocateBlock(uint8_t* block);
+
+	protected:
+		uint8_t* memoryBuffer;
+		uint64_t memoryBufferSize;
+		uint64_t blockSize;
+		std::vector<uint64_t> blockStack;
+		uint64_t blockStackTop;
+	};
+
+	/**
+	 * This class offers a convenient way to utilize a stack heap
+	 * to manage class instances.  Constructors and destructors
+	 * are called on allocation and deallocation, respectively.
+	 */
+	template<typename T>
+	class THEBE_API ObjectHeap : public StackHeap
+	{
+	public:
+		ObjectHeap()
+		{
+		}
+
+		virtual ~ObjectHeap()
+		{
+		}
+
+		T* Allocate()
+		{
+			THEBE_ASSERT(this->blockSize >= sizeof(T));
+
+			uint8_t* block = this->AllocateBlock();
+			if (!block)
+				return false;
+
+			T* object = new (block) T();
+			return object;
+		}
+
+		bool Deallocate(T* object)
+		{
+			auto block = reinterpret_cast<uint8_t*>(object);
+			if (!this->DeallocateBlock(block))
+				return false;
+
+			object->~T();
+			return true;
+		}
+	};
+}
