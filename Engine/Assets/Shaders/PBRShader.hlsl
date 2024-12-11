32b61fb4ded48a557727d6c7d44e321f4cba7472
Spencer T. Parkin <spencer.parkin@proton.me>
2024-12-10 17:04:25 -0700

    Start the PBR shader.

---
+++ Engine/Assets/Shaders/PBRShader.hlsl
@@ -0,0 +1,48 @@
+// PBRShader.hlsl
+
+cbuffer Constants : register(b0)
+{
+    float4x4 objToProj;
+    float4x4 objToCam;
+    float3 unitLightDir;
+};
+
+SamplerState generalSampler : register(s0);
+
+Texture2D albedoTexture : register(t0);
+Texture2D metalicTexture : register(t1);
+Texture2D roughnessTexture : register(t2);
+Texture2D normalTexture : register(t3);
+
+struct VSInput
+{
+    float3 position : POSITION;
+    float3 normal : NORMAL;
+    float2 texCoords : TEXCOORD;
+    float3 tangent : TEXCOORD1;
+};
+
+struct PSInput
+{
+    float4 position : SV_POSITION;
+    float3 normal : NORMAL;
+    float2 texCoords : TEXCOORD;
+    float3 tangent : TEXCOORD1;
+};
+
+PSInput VSMain(VSInput input)
+{
+    PSInput output;
+    
+    output.position = mul(objToProj, float4(input.position, 1.0));
+    output.normal = normalize(mul(objToCam, float4(input.normal, 0.0)).xyz);
+    output.tangent = normalize(mul(objToCam, float4(input.tangent, 0.0)).xyz);
+    output.texCoords = input.texCoords;
+    
+    return output;
+}
+
+float4 PSMain(PSInput input) : SV_TARGET
+{
+    return albedoTexture.Sample(generalSampler, input.texCoords);
+}
--- Engine/Assets/Shaders/PBRShader.shader
+++ Engine/Assets/Shaders/PBRShader.shader
@@ -9,16 +9,24 @@
             "offset": 0,
             "size": 64,
             "type": "float4x4"
+        },
+        "unitLightDir": {
+            "offset": 128,
+            "size": 12,
+            "type": "float3"
         }
     },
     "ps_entry_point": "PSMain",
     "ps_model": "ps_6_5",
-    "ps_shader_object": "Shaders/BasicShaderPS.dxbc",
-    "shader_code": "Shaders/BasicShader.hlsl",
+    "ps_shader_object": "Shaders/PBRShaderPS.dxbc",
+    "shader_code": "Shaders/PBRShader.hlsl",
     "texture_register_map": {
-        "diffuse": 0
+        "albedo": 0,
+        "metalic": 1,
+        "normal": 3,
+        "roughness": 2
     },
     "vs_entry_point": "VSMain",
     "vs_model": "vs_6_5",
-    "vs_shader_object": "Shaders/BasicShaderVS.dxbc"
+    "vs_shader_object": "Shaders/PBRShaderVS.dxbc"
 }
