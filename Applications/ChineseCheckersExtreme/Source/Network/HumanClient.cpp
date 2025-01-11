a3ddf3e729ea1541bacde677c551fc9aa7d98005
Spencer T. Parkin <spencer.parkin@proton.me>
2025-01-10 20:41:16 -0700

    Stub where to start loading in the game board mesh and mesh instances.

--- Applications/ChineseCheckersExtreme/Source/Network/HumanClient.cpp
+++ Applications/ChineseCheckersExtreme/Source/Network/HumanClient.cpp
@@ -10,10 +10,16 @@ HumanClient::HumanClient()
 
 /*virtual*/ bool HumanClient::HandleResponse(const ParseParty::JsonValue* jsonResponse)
 {
+	using namespace ParseParty;
+
 	if (!ChineseCheckersClient::HandleResponse(jsonResponse))
 		return false;
 
-	// TODO: Update the scene accordingly.
+	std::string response = ((const JsonString*)((const JsonObject*)jsonResponse)->GetValue("response"))->GetValue();
+	if (response == "get_game_state")
+	{
+		// TODO: Load all mesh and mesh instances into the scene.
+	}
 
 	return true;
 }
